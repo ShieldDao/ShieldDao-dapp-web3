5aa0d1448b9c59014e815eb9112598f2826c4eac
Amin A. Rezapour <amin.pe94@gmail.com>
2021-01-17 20:08:48 +0330

    sidebar submenus implemented

--- src/components/sidebar/NavItem.js
+++ src/components/sidebar/NavItem.js
@@ -1,20 +1,54 @@
+import { useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import { ReactComponent as ArrowDownIcon } from '../../static/svg/arrow-down.svg';
 
 const StyledButtonText = styled.span`
   flex-grow: 1;
   margin-left: 8px;
 `;
 
-const NavItem = ({ isOpen, href, title, icon, children, maxHeight }) => {
+const NavItem = ({ initIsOpen, href, title, icon, children, maxHeight }) => {
   const { pathname } = useLocation();
+  const [isOpen, setIsOpen] = useState(initIsOpen);
 
   return (
-    <li role="button">
-      <NavLink isActive={() => pathname === href} to={href}>
+    <li role="button" className={`${isOpen ? 'shown' : ''}`}>
+      <NavLink
+        onClick={(e) => {
+          if (children?.length) {
+            e.preventDefault();
+            setIsOpen(!isOpen);
+          }
+        }}
+        isActive={() => pathname === href}
+        to={href}
+      >
         {icon}
         <StyledButtonText>{title}</StyledButtonText>
+        {children?.length && <ArrowDownIcon className="icon arrow-down" />}
       </NavLink>
+
+      {children?.length && (
+        <ul>
+          {children.map((child, i) => (
+            <li key={i}>
+              {child.href.substr(0, 4) === 'http' ? (
+                <a href={child.href} rel="noreferrer" target="_blank">
+                  {child.title}
+                </a>
+              ) : (
+                <NavLink
+                  isActive={() => pathname === child.href}
+                  to={child.href}
+                >
+                  <StyledButtonText>{child.title}</StyledButtonText>
+                </NavLink>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </li>
   );
 };
--- src/components/sidebar/NavLinks.js
+++ src/components/sidebar/NavLinks.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { ReactComponent as HomeIcon } from '../../static/svg/home.svg';
 import { ReactComponent as ShieldIcon } from '../../static/svg/shield.svg';
+import { ReactComponent as MoreIcon } from '../../static/svg/menu.svg';
+import { ReactComponent as StatsIcon } from '../../static/svg/graph.svg';
 import NavItem from './NavItem';
 
 const StyledNav = styled.nav`
@@ -14,6 +16,16 @@ const StyledNav = styled.nav`
     li:not(.shown) > ul {
       display: none;
     }
+
+    li.shown svg.arrow-down {
+      transform: scale(1, -1);
+    }
+
+    li ul a {
+      padding-left: 35px;
+      height: 38px;
+      font-size: 13px;
+    }
   }
 
   a {
@@ -23,6 +35,7 @@ const StyledNav = styled.nav`
     line-height: 1;
     align-items: center;
     padding: 0 16px;
+    font-size: 14px;
 
     &.active {
       box-shadow: rgb(31, 199, 212) -4px 0px 0px inset;
@@ -32,6 +45,11 @@ const StyledNav = styled.nav`
     svg {
       width: 1.7em;
       height: 1.7em;
+
+      &.arrow-down {
+        width: 1em;
+        height: 1em;
+      }
     }
 
     &:hover {
@@ -44,8 +62,46 @@ const NavLinks = () => {
   return (
     <StyledNav>
       <ul>
-        <NavItem href="/" icon={<HomeIcon />} title="Home" />
-        <NavItem href="/vaults" icon={<ShieldIcon />} title="Vaults" />
+        <NavItem href="/" icon={<HomeIcon className="icon" />} title="Home" />
+        <NavItem
+          href="/vaults"
+          icon={<ShieldIcon className="icon" />}
+          title="Vaults"
+        />
+        <NavItem
+          href="/"
+          icon={<StatsIcon className="icon" />}
+          title="Info"
+          children={[
+            {
+              title: 'ShieldDao Website',
+              href: 'https://shielddao.app',
+            },
+          ]}
+        />
+        <NavItem
+          href="/"
+          icon={<MoreIcon className="icon" />}
+          title="More"
+          children={[
+            {
+              title: 'ShieldDao Website',
+              href: 'https://shielddao.app',
+            },
+            {
+              title: 'Github',
+              href: 'https://github.com/shieldDao/',
+            },
+            {
+              title: 'Docs',
+              href: 'https://docs.shielddao.app',
+            },
+            {
+              title: 'Blog',
+              href: 'https://shielddao.medium.com',
+            },
+          ]}
+        />
       </ul>
     </StyledNav>
   );
